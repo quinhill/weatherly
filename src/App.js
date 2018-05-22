aa538d61d7bc7045205100bc854003ff9983ca52
Quin Hill <quinhill@icloud.com>
2018-05-22 16:13:50 -0600

    Add location from mockdata

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Current from './Current.js';
+import {currentWeather} from './mockData.js';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>yo</h1>
+        <Current
+          city={currentWeather.location}
+          condition={currentWeather.currentCondition} />
       </div>
     );
   }
--- src/Current.js
+++ src/Current.js
@@ -1,1 +1,16 @@
-import React, { component } from 'react'
+import React, { component } from 'react';
+
+
+const Current = (props) => {
+
+
+    return (
+      <div>
+        <h2>{props.city}</h2>
+        <h3>{props.condition}</h3>
+      </div>
+    )
+  
+}
+
+export default Current;
