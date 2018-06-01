643cafa0f71b49da225b4ceebd68a194132ca8b3
Quin Hill <quinhill@icloud.com>
2018-06-01 10:11:22 -0600

    Fix apiCleaner tests

--- src/apiCleaner.test.js
+++ src/apiCleaner.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import apiCleaner from './apiCleaner';
-import mockData from './mockData';
+import { currentCleaner, hourlyCleaner, tenDayCleaner } from './apiCleaner';
+import data from './mockData';
 
 describe('apiCleaner', () => {
 
@@ -15,7 +15,7 @@ describe('apiCleaner', () => {
         visibility: '10.0',
         wind: 1.1,
         location: 'Denver, CO',
-        weekday: 'Thu',
+        weekday: 'Thursday',
         month: 'May',
         day: '24',
         summary: 'Sunny to partly cloudy. High 84F. Winds ENE at 5 to 10 mph.',
@@ -25,7 +25,7 @@ describe('apiCleaner', () => {
         icon: '/svg/clear.svg'
       }
     
-      const actualObject = apiCleaner.currentCleaner(mockData);
+      const actualObject = currentCleaner(data);
     
       expect(actualObject).toEqual(expectedObject);
     })
@@ -36,16 +36,16 @@ describe('apiCleaner', () => {
     
     it('should take in data and return an array of objects with the information needed for the Hourly component', () => {
       const expectedArray = [
-        { time: '10:00 AM', temperature: '72.3', icon: 'http://icons.wxug.com/i/c/k/clear.gif' },
-        { time: '11:00 AM', temperature: '75.8', icon: 'http://icons.wxug.com/i/c/k/clear.gif' },
-        { time: '12:00 PM', temperature: '78.1', icon: 'http://icons.wxug.com/i/c/k/clear.gif' },
-        { time: '1:00 PM', temperature: '79.9', icon: 'http://icons.wxug.com/i/c/k/clear.gif' },
-        { time: '2:00 PM', temperature: '81.4', icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' },
-        { time: '3:00 PM', temperature: '82.4', icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' },
-        { time: '4:00 PM', temperature: '82.4', icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' }
+        { time: '10:00 AM', temperature: '72.3', icon: '/svg/clear.svg' },
+        { time: '11:00 AM', temperature: '75.8', icon: '/svg/clear.svg' },
+        { time: '12:00 PM', temperature: '78.1', icon: '/svg/clear.svg' },
+        { time: '1:00 PM', temperature: '79.9', icon: '/svg/clear.svg' },
+        { time: '2:00 PM', temperature: '81.4', icon: '/svg/partlycloudy.svg' },
+        { time: '3:00 PM', temperature: '82.4', icon: '/svg/partlycloudy.svg' },
+        { time: '4:00 PM', temperature: '82.4', icon: '/svg/partlycloudy.svg' }
       ]
     
-      const actualArray = apiCleaner.hourlyCleaner(mockData)
+      const actualArray = hourlyCleaner(data)
 
       expect(actualArray).toEqual(expectedArray);
     })
@@ -55,19 +55,19 @@ describe('apiCleaner', () => {
     
     it('should take in data and return an array of objects with the information needed for the Hourly component', () => {
       const expectedArray = [
-        { weekday: 'Thursday', daysHigh: '84', daysLow: '54', icon: 'clear'  },
-        { weekday: 'Friday', daysHigh: '88', daysLow: '57', icon: 'clear'  },
-        { weekday: 'Saturday', daysHigh: '92', daysLow: '57', icon: 'clear'  },
-        { weekday: 'Sunday', daysHigh: '88', daysLow: '56', icon: 'clear'  },
-        { weekday: 'Monday', daysHigh: '76', daysLow: '54', icon: 'chancetstorms' },
-        { weekday: 'Tuesday', daysHigh: '79', daysLow: '53', icon: 'clear' },
-        { weekday: 'Wednesday', daysHigh: '83', daysLow: '55', icon: 'clear' },
-        { weekday: 'Thursday', daysHigh: '85', daysLow: '55', icon: 'clear' },
-        { weekday: 'Friday', daysHigh: '86', daysLow: '56', icon: 'clear' },
-        { weekday: 'Saturday', daysHigh: '79', daysLow: '56', icon: 'clear' }
+        { weekday: 'Thursday', daysHigh: '84', daysLow: '54', icon: '/svg/clear.svg'  },
+        { weekday: 'Friday', daysHigh: '88', daysLow: '57', icon: '/svg/clear.svg'  },
+        { weekday: 'Saturday', daysHigh: '92', daysLow: '57', icon: '/svg/clear.svg'  },
+        { weekday: 'Sunday', daysHigh: '88', daysLow: '56', icon: '/svg/clear.svg'  },
+        { weekday: 'Monday', daysHigh: '76', daysLow: '54', icon: '/svg/chancetstorms.svg' },
+        { weekday: 'Tuesday', daysHigh: '79', daysLow: '53', icon: '/svg/clear.svg' },
+        { weekday: 'Wednesday', daysHigh: '83', daysLow: '55', icon: '/svg/clear.svg' },
+        { weekday: 'Thursday', daysHigh: '85', daysLow: '55', icon: '/svg/clear.svg' },
+        { weekday: 'Friday', daysHigh: '86', daysLow: '56', icon: '/svg/clear.svg' },
+        { weekday: 'Saturday', daysHigh: '79', daysLow: '56', icon: '/svg/clear.svg' }
       ]
       
-      const actualArray = apiCleaner.tenDayCleaner(mockData);
+      const actualArray = tenDayCleaner(data);
       
       expect(actualArray).toEqual(expectedArray);
     })
